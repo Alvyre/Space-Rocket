8351035a89047beb24418c5832ff9b912d7ba148
Pierre Charles <pierre.chls@gmail.com>
2016-05-20 18:16:57 +0200

    Fix JavaDOC

--- src/imac/MainApp.java
+++ src/imac/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends PApplet {
 	
 	/**
 	 * Main method of the app
-	 * @param String
+	 * @param args string
 	 * @since 1.0
 	 */
 	public static void main(String[] args){
